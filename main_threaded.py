577ef5034794fa88b7be6848f0d5512e3615e6be
3022-2 <82278708+3022-2@users.noreply.github.com>
2024-03-19 01:37:03 +0000

    Update main_threaded.py

--- main_threaded.py
+++ main_threaded.py
@@ -7,34 +7,7 @@ import aiohttp
 
 #before running for the first time remember to set your headers
 
-headers = {
-    "authority": "www.reversephonelookup.com",
-    "method": "GET",
-    "path": "/number/7149967075/",
-    "scheme": "https",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-    "Cache-Control": "max-age=0",
-    "Cookie": "device-id=d87b4868-db09-4698-b840-037e72d88fb8; _gcl_au=1.1.67196235.1710808036; _gid=GA1.2.601514972.1710808036; last-known-device-id=d87b4868-db09-4698-b840-037e72d88fb8; cf_chl_3=3d019c99636adec; cf_clearance=b0tkjmRY0qwqcfxTp9zmaYTVO30p3XOrkQpPxkVKcAA-1710810043-1.0.1.1-W.yQaJjz6Ro3t28mi7RxqwF3HL0VC3rMF4Bovv2OSWQZFtHVzZj1x3d6WPT_pH.WROmH1jtdJZjA7hTEog8Thw; PHPSESSID=1720u598qr0j7rb71kit330rv8; sessionId=2d97c90b-49de-454a-98ea-dc31cce37f8d; sessionCreated=2024-03-19T01%3A03%3A35%2B00%3A00; _ga_0V7NXWZ64P=GS1.1.1710808036.1.1.1710810309.60.0.0; _ga=GA1.2.1972488876.1710808036; _gat_UA-74882607-7=1",
-    "Sec-Ch-Ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
-    "Sec-Ch-Ua-Arch": "x86",
-    "Sec-Ch-Ua-Bitness": "64",
-    "Sec-Ch-Ua-Full-Version": "122.0.6261.129",
-    "Sec-Ch-Ua-Full-Version-List": "\"Chromium\";v=\"122.0.6261.129\", \"Not(A:Brand\";v=\"24.0.0.0\", \"Google Chrome\";v=\"122.0.6261.129\"",
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Model": "\"\"",
-    "Sec-Ch-Ua-Platform": "\"Windows\"",
-    "Sec-Ch-Ua-Platform-Version": "\"15.0.0\"",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0"
-}
-
-
+headers = {}
 
 async def request_site(session, current_number):
     try:
