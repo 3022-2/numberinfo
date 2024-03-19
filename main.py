350bc0abe28bcf7ad2270296ce9062512e4b3fd6
3022-2 <82278708+3022-2@users.noreply.github.com>
2024-03-19 09:20:01 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,26 @@ import os
 
 #before running for the first time remember to set your headers
 
-headers = {}
+headers = {
+    "authority": "www.reversephonelookup.com",
+    "method": "GET",
+    "path": "/number/7149967004/",
+    "scheme": "https",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Cache-Control": "max-age=0",
+    "Cookie": "device-id=535305ac-4101-4823-a6d7-bf3ec6fc67b1; _gcl_au=1.1.199284598.1708454405; last-known-device-id=535305ac-4101-4823-a6d7-bf3ec6fc67b1; PHPSESSID=6kvi0agg73qp23s1uk7n5r9p7g; sessionId=559bb42f-bdee-4f63-bb6a-d98880ca708c; sessionCreated=2024-03-05T21%3A20%3A12%2B00%3A00; _gid=GA1.2.1592401715.1709673614; cf_clearance=udXI1x_jpI_ommg7XM8KwyiLvpY.UU399kersDS_AfM-1709673614-1.0.1.1-10FYKtfVreeyRgw9JlHZFHaD6WscWO1FHVAzKijXbnV6LzxEzs728Rj5B0W8ev9lv_hx_VpT.hFEV4nZZ3DpSw; _ga_0V7NXWZ64P=GS1.1.1709673614.22.1.1709674256.50.0.0; _ga=GA1.2.707113878.1708454406",
+    "Sec-Ch-Ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+}
 
 cwd = os.getcwd()
 
@@ -50,7 +69,7 @@ def request_sites(current_number):
                 ownername = soup.find("h3")
                 generalinfo = soup.find("ul")
                 if generalinfo and ownername:
-                    with open("data/AT&T_data.txt", "a") as o:
+                    with open("data/non_AT&T_data.txt", "a") as o:
                         if ownername and generalinfo:
                             owner_name = ownername.get_text().strip()
                             o.write(f"{current_number} - {owner_name}\n")
@@ -72,7 +91,7 @@ def request_sites(current_number):
                     print(Fore.RED + f"{current_number} || Element not found.")
             else:
                 if r.status_code == 403:
-                    print(Fore.RED + f"{current_number} || ERROR || STATUS CODE 403 - POSSIBLE FIX: UPDATE HEADERS - IF THAT DOESNT WORK GET NEW IP")
+                    print(Fore.RED + f"{current_number} || ERROR || STATUS CODE 403 - POSSIBLE FIX: UPDATE HEADERS - IF THIS DOESNT FIX CHANGE IP AND GET NEW HEADERS")
                 else:
                     print(Fore.RED + f"{current_number} || INVALID || DOESNT EXIST")        
         except Exception as e:
